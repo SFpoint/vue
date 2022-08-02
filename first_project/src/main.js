c77d7ed7ae576ed44b2ca97f43286c96be973ae0
Sergey <vendettarock2@gmail.com>
2022-08-02 13:03:11 +0300

    add routing and models

--- first_project/src/main.js
+++ first_project/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueResource from "vue-resource";
 import VueRouter from "vue-router";
-import Routes from "./routes";
+import Routes from "./router/routes";
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
---
+++ first_project/src/models/blog.js
@@ -0,0 +1,8 @@
+export class blog {
+  constructor() {
+    this.title = null;
+    this.content = null;
+    this.categories = null;
+    this.author = null;
+  }
+}
